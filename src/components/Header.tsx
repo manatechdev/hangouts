00dae79fd907d0b22d2a8170def2486fca3aea32
MacKay Killian <mackay.killian@gmail.com>
2023-11-14 12:54:54 -0700

    added account button to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,21 @@
+import Button from "./Button";
+import { UserCircleIcon } from "@heroicons/react/20/solid";
+import styled from "@emotion/styled";
+
+const HEADER_CLASSES = "container bg-hred";
+
 function Header() {
-  return <div>TEST!!</div>;
+  return (
+    <div className={HEADER_CLASSES}>
+      <Button
+        size="sm"
+        variant="ghost"
+        label="Account"
+        iconStart={<UserCircleIcon />}
+        customStyle="bg-opacity-0"
+      />
+    </div>
+  );
 }
 
 export default Header;
