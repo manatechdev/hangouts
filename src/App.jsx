12b7ee619fc6862e3370034996c9c877a3b8c311
MacKay Killian <mackay.killian@gmail.com>
2023-11-13 17:14:59 -0700

    Removed default App.js page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
+import { BeakerIcon } from "@heroicons/react/24/solid";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>TEST!!</div>;
+}
+
+export default Header;
