671a7f3434450dfdaa20a485b823cb8558895e03
MacKay Killian <mackay.killian@gmail.com>
2023-11-16 14:42:44 -0700

    sorted classes using Headwind extension

--- src/components/SideNavItem.tsx
+++ src/components/SideNavItem.tsx
@@ -16,7 +16,7 @@ function SideNavItem({ label, icon, selected }: SideNavItemProps) {
   return (
     <div className="w-[164px] h-10 flex flex-row items-center rounded-lg hover:bg-hyellow-50">
       <div className={`w-1 h-6 rounded ${selected && "bg-hyellow-700"}`}></div>
-      <div className="w-40 h-10 p-2 flex flex-row text-hgray-700">
+      <div className="flex flex-row w-40 h-10 p-2 text-hgray-700">
         <Icon type={icon} customStyle="w-6 h-6" />
         <div className="ml-2 text-lg font-medium">{label}</div>
       </div>
