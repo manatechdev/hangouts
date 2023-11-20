4c9339ca912d3e4480d3d7ac368ffce76b6f6d6c
MacKay Killian <mackay.killian@gmail.com>
2023-11-20 12:47:04 -0700

    style changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import SideNav from "./SideNav";
 import SideNavItem from "./SideNavItem";
 
@@ -11,15 +11,25 @@ function Sidebar({ children }: SidebarProps) {
   return (
     <div className="w-1/4 pt-4 pb-4 pb-r bg-hred-100">
       <SideNav>
-        <Link to="/">
-          <SideNavItem icon="calendarDays" label="Upcoming" selected />
-        </Link>
-        <Link to="/community">
-          <SideNavItem icon="users" label="Community" />
-        </Link>
-        <Link to="/settings">
-          <SideNavItem icon="cog" label="Settings" />
-        </Link>
+        <NavLink to="/">
+          {({ isActive }) => (
+            <SideNavItem
+              icon="calendarDays"
+              label="Upcoming"
+              selected={isActive}
+            />
+          )}
+        </NavLink>
+        <NavLink to="/community">
+          {({ isActive }) => (
+            <SideNavItem icon="users" label="Community" selected={isActive} />
+          )}
+        </NavLink>
+        <NavLink to="/settings">
+          {({ isActive }) => (
+            <SideNavItem icon="cog" label="Settings" selected={isActive} />
+          )}
+        </NavLink>
       </SideNav>
     </div>
   );
