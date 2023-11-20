819827a664e10edae39185a4d81f456a4c73ead3
MacKay Killian <mackay.killian@gmail.com>
2023-11-20 11:54:26 -0700

    added EventTiles component and put in mock data for now

--- src/components/EventTile.tsx
+++ src/components/EventTile.tsx
@@ -1,13 +1,38 @@
 import { ReactNode } from "react";
+import Icon from "./Icon";
 
-interface EventTile {}
+interface EventTileProps {
+  event?: any;
+}
 
-function EventTile() {
+function EventTile({ event }: EventTileProps) {
+  let { title, dateTime } = event;
   return (
-    <div className="h-24 p-6 bg-hpurple-500">
-      <div className="flex flex-row justify-between h-12 bg-hgreen-700">
-        <div className="w-24 h-12 bg-hred-700"></div>
-        <div className="w-24 h-12 bg-hred-700"></div>
+    <div className="h-24 p-6 mb-4 rounded-md shadow border-hgray-400">
+      <div className="flex flex-row justify-between h-12">
+        <div className="flex flex-row h-12">
+          <div className="w-12 h-12 rounded bg-hgray-400">
+            <img></img>
+          </div>
+          <div className="flex flex-col ml-4">
+            <div className="h-6 font-semibold">{title}</div>
+            <div className="h-6 text-left">{dateTime}</div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-end h-12">
+          <div className="flex flex-row h-8">
+            <div className="w-8 h-8 border-[5px] rounded-full border-hgray-200 bg-hgray-400"></div>
+            <div className="w-8 h-8 -translate-x-2 border-[5px] rounded-full border-hgray-200 bg-hgray-400"></div>
+            <div className="w-8 h-8 -translate-x-4 border-[5px] rounded-full border-hgray-200 bg-hgray-400"></div>
+            <div className="w-8 h-8 -translate-x-6 border-[5px] rounded-full border-hgray-200 bg-hgray-400"></div>
+          </div>
+          <div className="flex flex-row w-8 h-8">
+            <Icon
+              type="informationCircle"
+              customStyle="w-7 h-7 text-hgray-600"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,8 +1,27 @@
-import UnderDevelopment from "../components/UnderDevelopment";
+import EventTile from "../components/EventTile";
+
+const mockEventList = [
+  {
+    title: "Dinner w. Book Club",
+    dateTime: "Nov 24, 2023 7:00pm",
+  },
+  {
+    title: "Friendsgiving Lunch Party",
+    dateTime: "Nov 27, 2023 11:00am",
+  },
+  {
+    title: "Shopping Trip and Cafe Study Session",
+    dateTime: "Dec 18, 2023 11:30am",
+  },
+];
 
 function Dashboard() {
   return (
-    <UnderDevelopment message="The Dashboard is still being developed. Check again soon!" />
+    <div className="pt-8">
+      {mockEventList.map((item, i) => {
+        return <EventTile key={i} event={item}></EventTile>;
+      })}
+    </div>
   );
 }
 
